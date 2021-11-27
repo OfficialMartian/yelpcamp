af72665d150b665db7dab8a1fa7e19964e0b582c
OfficialMartian <caraj0503@gmail.com>
2021-11-27 14:03:48 +0300

    fixed register error

--- controllers/users.js
+++ controllers/users.js
@@ -9,14 +9,12 @@ module.exports.registerUser = async(req, res, next) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
+        req.login(registeredUser, (err) => {
             if (err)
                 return next(err);
             req.flash('success', 'Welcome to YelpCamp');
             res.redirect('/campgrounds');
         })
-        req.flash('success', 'Welcome to YelpCamp');
-        res.redirect('/campgrounds');
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
